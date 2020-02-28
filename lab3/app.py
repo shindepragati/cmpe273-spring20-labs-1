9fcc8acceb08f5144d1dc965e679b37fa3c0ad6b
shindepragati <60525827+shindepragati@users.noreply.github.com>
2020-02-28 01:00:35 -0800

    Add files via upload

---
+++ lab3/app.py
@@ -0,0 +1,138 @@
+from ariadne import QueryType, graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, MutationType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, request, jsonify
+import time
+
+
+app = Flask(__name__)
+
+DB={
+    'students':{},
+    'classes':{'student':[]}
+}
+
+type_defs = load_schema_from_path('schema.graphql')
+query = QueryType()
+mutation = MutationType()
+
+
+@query.field("hello")
+def hello(_, info):
+    request = info.context
+    user_agent = request.headers.get("User-Agent", "Guest")
+    return "Hello, %s!" % user_agent
+
+@query.field("createStudent")
+@mutation.field("createStudent")
+def createStudent(_, info, name):
+    #req = info.context
+    n=name
+    did=int(time.time())
+    DB['students'].update({did : n})
+    student = { 
+        "id" : did,
+        "name" : n
+    }
+    print(DB)
+    return student
+
+
+@query.field("getStudentDetails")
+def getStudentDetails(_, info, id):
+    data = DB['students']
+    temp=""
+    for i in data.keys():
+        print(i)
+        if int(id)==i:
+            temp=data[i]
+            break
+
+    if(temp!=""):
+        student = { 
+        "id" : id,
+        "name" : temp
+        }     
+
+    return student 
+
+@query.field("createClass")
+@mutation.field("createClass")
+def createClass(_, info, name):
+    
+    did=int(time.time())
+   
+    DB['classes'].update({did : name})
+    classes = { 
+        "id" : did,
+        "name" : name
+    }
+    
+    print(DB)
+    return classes 
+
+@query.field("getClassDetails")
+def getClassDetails(_, info, id):
+    temp=""
+    data = DB['classes']
+    for i in data.keys():
+        print(i)
+        if int(id)==i:
+            temp=data[i]
+            break
+
+    if(temp!=""):
+        classes = { 
+            "id" : id,
+            "name" : temp
+        }
+    return classes 
+
+@query.field("addStudenttoClass")
+@mutation.field("addStudenttoClass")
+def addStudenttoClass(_, info, s_id,class_id):
+    stud_data = DB['students']
+    class_data = DB['classes']
+    for i in stud_data.keys():
+        if int(s_id)==i:
+            temp=stud_data[i]
+            did=class_id
+            name="CMPE272"
+            class_data['student'].append({s_id:temp})
+            DB['classes'].update({did : name})
+            classes={"id":did,"name":name,"student":class_data['student']}
+            break
+        
+    return classes 
+
+schema = make_executable_schema(type_defs, [query, mutation])
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playgroud():
+    # On GET request serve GraphQL Playground
+    # You don't need to provide Playground if you don't want to
+    # but keep on mind this will not prohibit clients from
+    # exploring your API using desktop GraphQL Playground app.
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    # GraphQL queries are always sent as POST
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    #print(data)
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    #print(result)
+    status_code = 200 if success else 400
+    return result, status_code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
